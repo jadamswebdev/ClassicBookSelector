98ee8003b1eddb20b3f18ad85340fb45209cc9f6
jadamswebdev <60942973+jadamswebdev@users.noreply.github.com>
2020-02-11 14:28:04 -0600

    Add files via upload

---
+++ ClassicBookSelector/ClassicBookSelector/BookSelector.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ClassicBookSelector
+{
+    partial class BookSelector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BookSelector));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.readingLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.desLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Great Expectation",
+            "1984",
+            "To Kill a Mockingbird",
+            "Animal Farm",
+            "The Great Gatsby"});
+            this.listBox1.Location = new System.Drawing.Point(47, 127);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // readingLabel
+            // 
+            this.readingLabel.AutoSize = true;
+            this.readingLabel.Location = new System.Drawing.Point(44, 287);
+            this.readingLabel.Name = "readingLabel";
+            this.readingLabel.Size = new System.Drawing.Size(412, 52);
+            this.readingLabel.TabIndex = 1;
+            this.readingLabel.Text = resources.GetString("readingLabel.Text");
+            this.readingLabel.Visible = false;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Lucida Console", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.SeaGreen;
+            this.titleLabel.Location = new System.Drawing.Point(184, 36);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(369, 27);
+            this.titleLabel.TabIndex = 2;
+            this.titleLabel.Text = "Classic Book Selector";
+            // 
+            // desLabel
+            // 
+            this.desLabel.AutoSize = true;
+            this.desLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.desLabel.Location = new System.Drawing.Point(228, 127);
+            this.desLabel.Name = "desLabel";
+            this.desLabel.Size = new System.Drawing.Size(51, 16);
+            this.desLabel.TabIndex = 3;
+            this.desLabel.Text = "label1";
+            this.desLabel.Click += new System.EventHandler(this.desLabel_Click);
+            // 
+            // BookSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 450);
+            this.Controls.Add(this.desLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.readingLabel);
+            this.Controls.Add(this.listBox1);
+            this.Name = "BookSelector";
+            this.Text = "Classic Book Selector";
+            this.Load += new System.EventHandler(this.BookSelector_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label readingLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label desLabel;
+    }
+}
+
---
+++ ClassicBookSelector/ClassicBookSelector/BookSelector.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassicBookSelector
+{
+    public partial class BookSelector : Form
+    {
+        public BookSelector()
+        {
+            InitializeComponent();
+
+            //Prompts and event handler from when the mouse cursor enters the listbox1
+            listBox1.MouseEnter += new System.EventHandler(listBox1_MouseEnter);
+            listBox1.MouseLeave += new System.EventHandler(listBox1_MouseLeave);
+
+        }
+
+        private void BookSelector_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listbox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == 1)
+            {
+                desLabel.Text = "Great Expectations follows the young protagonist Pip, a lower-class orphan who lives with his sister and her husband in Kent. ... " +
+                    "            Pip becomes enamored with Estella, Miss Havisham's young ward. Estella is beautiful yet cruel, " +
+                    "            and Pip makes it his life's mission to become a wealthy gentleman worthy of being her husband. ";
+            }
+        }
+
+        private void listBox1_MouseEnter(object sender, System.EventArgs e)
+        {
+
+            readingLabel.Visible = true; 
+        }
+
+        private void listBox1_MouseLeave(object sender, System.EventArgs e)
+        {
+
+            readingLabel.Visible = false;
+        }
+
+        private void desLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == 0)
+            {
+                desLabel.Text = "Great Expectations follows the young protagonist Pip, a lower-class orphan\n " +
+                                "who lives with his sister and her husband in Kent. Pip becomes enamored with \n" +
+                                "Estella, Miss Havisham's young ward. Estella is beautiful yet cruel,\n " +
+                                "and Pip makes it his life's mission to become a wealthy gentleman worthy \n" +
+                                "of being her husband. ";
+
+                this.BackColor = Color.Coral;
+            }
+            if (listBox1.SelectedIndex == 1)
+            {
+                desLabel.Text = "1984 is a dystopian novella by George Orwell published in 1949, which follows\n " +
+                                "the life of  Winston Smith, a low ranking member of 'the Party', who is frus- \n" +
+                                "trated by the omnipresent eyes of the part, and its ominous ruler Big Brother.\n " +
+                                "'Big Brother' controls every aspect of people's lives. \n";
+
+                this.BackColor = Color.LightCoral;
+            }
+            if (listBox1.SelectedIndex == 2)
+            {
+                desLabel.Text = "To Kill a Mockingbird is the story of a young girl named Scout whose father\n " +
+                                "is a lawyer defending a black man accused of raping a white woman. She lives in \n" +
+                                "a small Southern town that is shaken by the trial, because the man could not\n " +
+                                "have physically commmitted the crime. \n";
+
+                this.BackColor = Color.LightBlue;
+            }
+            if (listBox1.SelectedIndex == 3)
+            {
+                desLabel.Text = "The book tells the story of a group of farm animals who rebel against their\n " +
+                                "human farmer, hoping to create a society where the animals can be equal, free, \n" +
+                                "and happy. Ultimately, however, the rebellion is betrayed and the farm ends up\n " +
+                                "in a state as bad as it was before, under the dictatorship of a pig named - \n" +
+                                "Napoleon ";
+
+                this.BackColor = Color.LightGreen;
+            }
+            if (listBox1.SelectedIndex == 4)
+            {
+                desLabel.Text = "The Great Gatsby, third novel by F. Scott Fitzgerald, published in 1925. Set in\n " +
+                                "Jazz Age New York, the novel tells the tragic story of Jay Gatsby, a self-made \n" +
+                                "millionaire, and his pursuit of Daisy Buchanan, a wealthy young woman whom he\n " +
+                                "love in his youth. \n";
+
+                this.BackColor = Color.LightPink;
+            }
+        }
+    }
+}
